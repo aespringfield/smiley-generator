433c0d0a1a4bf2b8f2099f116d8989586d77ab0a
Anna Springfield <anna.e.springfield@gmail.com>
2017-11-13 10:39:09 -0600

    set up enzyme and run some practice tests

---
+++ src/containers/SmileyGenerator.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SmileyGenerator extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {smiley: ''}
+    }
+    render() {
+        return <div></div>
+    }
+
+    returnFive() {
+        return 5;
+    }
+}
+
+export default SmileyGenerator;
